b648b00f74f2f2f5f63e237b5dd563ed96d1b2ac
Peter Listiak <peter@listiak.eu>
2017-10-04 10:47:17 +0200

    Reformatting code.

--- hpoo.py
+++ hpoo.py
@@ -6,123 +6,123 @@ import requests.packages.urllib3
 import time
 from base64 import encodestring
 
+
 class hpoo():
+    def __init__(self, config=None, flow=None):
 
-	def __init__(self, config = None, flow = None):
+        self.config = config
+        self.flow = flow
 
-		self.config = config
-		self.flow = flow
+        self.session = requests.Session()
+        self.session.url = self.config['url']
+        self.auth = encodestring(self.config['username'] + ":" + self.config['password'])
+        self.session.headers.update({'Authorization': 'Basic ' + self.auth.rstrip()})
+        self.session.headers.update({'Accept': 'application/json'})
 
-		self.session = requests.Session()
-		self.session.url = self.config['url']
-		self.auth = encodestring(self.config['username']+ ":" + self.config['password'])
-		self.session.headers.update({'Authorization': 'Basic '+ self.auth.rstrip()})
-		self.session.headers.update({'Accept':'application/json'})
+        self.error = None
+        self.run_id = None
+        self.status = None
+        self.flow_result = None
 
-		self.error = None
-		self.run_id = None
-		self.status = None
-		self.flow_result = None
+        if self.config['trustcert'] is True:
+            requests.packages.urllib3.disable_warnings()
 
-		if self.config['trustcert'] is True :
-			requests.packages.urllib3.disable_warnings() 
+    def run(self):
+        logging.info('Entering run_flow')
 
-	def run(self) :
-		logging.info('Entering run_flow')
+        # collect flow information
+        url = self.session.url + '/rest/v1/flows/' + self.flow['uuid']
+        r = self.session.get(url, verify=False)
 
-		#collect flow information
-		url = self.session.url +'/rest/v1/flows/'+ self.flow['uuid']
-		r = self.session.get(url, verify=False)
+        if (r.reason != 'OK'):
+            raise Exception(r.reason)
 
-		if (r.reason != 'OK') :
-			raise Exception(r.reason)
+        logging.debug(r.text)
+        flow_info = json.loads(r.text)
 
-		logging.debug(r.text)
-		flow_info = json.loads(r.text)
-	
-		#check mandatory flow inputs
-		url = self.session.url + '/rest/v1/flows/' + self.flow['uuid'] + '/inputs'
-		r = self.session.get(url,verify=False)
+        # check mandatory flow inputs
+        url = self.session.url + '/rest/v1/flows/' + self.flow['uuid'] + '/inputs'
+        r = self.session.get(url, verify=False)
 
-		if (r.reason != 'OK') :
-			raise Exception (r.reason)
+        if (r.reason != 'OK'):
+            raise Exception(r.reason)
 
-		logging.debug(r.text)
+        logging.debug(r.text)
 
-		if (r.text) :
-			flow_input = json.loads(r.text)
+        if (r.text):
+            flow_input = json.loads(r.text)
 
-		for i in flow_input:
-			if (i['mandatory'] is True and i['name'] not in self.flow['inputs']) :
-				raise Exception('Missing required flow input: ' + i['name'])
-	
-		#construct json post object
-		post_data = {}
-		post_data['uuid'] = self.flow['uuid']
-		post_data['runName'] = flow_info['name']
-		post_data['logLevel'] = 'DEBUG'
-		
-		if (input is not None) :
-			post_data['inputs']	 = self.flow['inputs']
+        for i in flow_input:
+            if (i['mandatory'] is True and i['name'] not in self.flow['inputs']):
+                raise Exception('Missing required flow input: ' + i['name'])
 
-		json_post = json.dumps(post_data)
+        # construct json post object
+        post_data = {}
+        post_data['uuid'] = self.flow['uuid']
+        post_data['runName'] = flow_info['name']
+        post_data['logLevel'] = 'DEBUG'
 
-		#run the flow
-		url = self.session.url + '/rest/v1/executions'
-		self.session.headers = {'Content-type':'application/json'}
-		r = self.session.post(url, data=json_post, verify=False)
-	
-		if (r.reason != 'Created') :
-			logging.debug(r.reason)
-			raise Exception (r.reason)
-		else :
-			response  = json.loads(r.text)
-			logging.debug(response)
-			self.run_id = response['executionId']
-			return response['executionId']	
+        if (input is not None):
+            post_data['inputs'] = self.flow['inputs']
 
-	def get_status(self) :
+        json_post = json.dumps(post_data)
 
-		logging.info('Getting execution summary')
+        # run the flow
+        url = self.session.url + '/rest/v1/executions'
+        self.session.headers = {'Content-type': 'application/json'}
+        r = self.session.post(url, data=json_post, verify=False)
 
-		url = self.session.url + '/rest/v1/executions/' + self.run_id + '/summary'
+        if (r.reason != 'Created'):
+            logging.debug(r.reason)
+            raise Exception(r.reason)
+        else:
+            response = json.loads(r.text)
+            logging.debug(response)
+            self.run_id = response['executionId']
+            return response['executionId']
 
-		r = self.session.get(url, verify=False)
-		if (r.reason != 'OK'):
-			raise Exception(r.reason)
-	
-		logging.debug(r.text)
-		response = json.loads(r.text)
+    def get_status(self):
 
-		return response[0]['resultStatusType']
+        logging.info('Getting execution summary')
 
-	def collect(self) :
+        url = self.session.url + '/rest/v1/executions/' + self.run_id + '/summary'
 
-		logging.info('Entering collect_result for runId '+self.run_id)
+        r = self.session.get(url, verify=False)
+        if (r.reason != 'OK'):
+            raise Exception(r.reason)
 
-		url = self.config['url'] + '/rest/v1/executions/' + self.run_id + '/execution-log'
+        logging.debug(r.text)
+        response = json.loads(r.text)
 
-		r = self.session.get(url, verify=False)
-		if (r.reason != 'OK') :
-			raise Exception(r.reason)
+        return response[0]['resultStatusType']
 
-		logging.debug(r.text)
-		result = json.loads(r.text)
-		self.flow_result = result
-		
-		return result
-	
-	def get_run_id(self) :
-		return self.run_id
+    def collect(self):
 
-	def get_error(self) :
-		return self.error
+        logging.info('Entering collect_result for runId ' + self.run_id)
 
-	def get_status(self) :
-		return self.status
+        url = self.config['url'] + '/rest/v1/executions/' + self.run_id + '/execution-log'
 
-	def get_flow_result(self) :
-		return self.flow_result
+        r = self.session.get(url, verify=False)
+        if (r.reason != 'OK'):
+            raise Exception(r.reason)
 
-	def get_flow(self) :
-		return self.flow
+        logging.debug(r.text)
+        result = json.loads(r.text)
+        self.flow_result = result
+
+        return result
+
+    def get_run_id(self):
+        return self.run_id
+
+    def get_error(self):
+        return self.error
+
+    def get_status(self):
+        return self.status
+
+    def get_flow_result(self):
+        return self.flow_result
+
+    def get_flow(self):
+        return self.flow
--- julep.py
+++ julep.py
@@ -9,139 +9,139 @@ import time
 import hpoo as oo
 from junit_xml import TestSuite, TestCase
 
-def main () :
 
-	levels = {
-		'debug': logging.DEBUG,
-		'info': logging.INFO,
-		'warning': logging.WARNING,
-		'error': logging.ERROR,
-		'critical': logging.CRITICAL
-	}
+def main():
+    levels = {
+        'debug': logging.DEBUG,
+        'info': logging.INFO,
+        'warning': logging.WARNING,
+        'error': logging.ERROR,
+        'critical': logging.CRITICAL
+    }
+
+    parser = argparse.ArgumentParser(description='HP Operation Orchestration testing tool')
+    parser.add_argument('--askpass', action='store_true', help='Ignore password from config file and prompt for it.')
+    parser.add_argument('--configfile', default='julep.yaml', help='Configfile with hpoo flow testcases')
+    parser.add_argument('--loglevel', default='INFO', help='FATAL, ERROR, WARNING, INFO, DEBUG')
+    parser.add_argument('--logfile', default='julep.log', help='Logfile to store messages (Default: julep.log)')
+    parser.add_argument('--timeout', default=3600, type=int,
+                        help='The time to wait for flow completion in seconds (Default: 3600 - 1hour)')
+    parser.add_argument('--heartbeat', default=120, type=int,
+                        help='Operation Orchestration polling interval (Default: 120 secs)')
+    parser.add_argument('--quiet', action='store_true', help='Do not print logging to stdout')
+    parser.add_argument('--trustcert', action='store_true', help='Trust self-signed certs')
+    parser.add_argument('--configfmt', default='yaml', help="Configfile format - json or yaml. Default json.")
+    parser.add_argument('--delay', default=15, type=int, help="Delay in seconds to wait between starting flows")
+    parser.add_argument('--junitoutput', default='julepout.xml',
+                        help="The location of the junit xml output. Default julepout.xml")
+
+    args = parser.parse_args()
+    loglevel = levels.get(args.loglevel, logging.NOTSET)
+    logging.basicConfig(
+        level=args.loglevel,
+        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+        datefmt='%m-%d %H:%M',
+        filename=args.logfile,
+        filemode='a')
+
+    root = logging.getLogger()
+
+    if args.quiet is False:
+        console = logging.StreamHandler()
+        console.setLevel(args.loglevel)
+
+        formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+        console.setFormatter(formatter)
+
+        root.addHandler(console)
+
+    logging.info("Want some blacksea julep?")
+    config = parse_config(args.configfile, args.configfmt)
+    config['general']['trustcert'] = args.trustcert
+
+    if args.askpass is True:
+        config['general']['password'] = getpass.getpass()
+
+    testcases = {
+        'running': [],
+        'finished': [],
+    }
+
+    for flow in config['flows']:
+        test = oo.hpoo(config['general'], flow)
+        name = test.run()
+        if args.delay is not None:
+            logging.info("sleeping between runs for %s secs", args.delay)
+            time.sleep(args.delay)
+        testcases['running'].append(test)
+
+    root.debug(testcases)
+
+    timeout = int(args.timeout)
+    heartbeat = int(args.heartbeat)
+
+    while timeout >= heartbeat:
+        logging.info('Tracking testcases in running state')
+        for test in testcases['running']:
+
+            if test.get_status() == 'RUNNING':
+                continue
+            else:
+                testcases['finished'].append(test)
+                testcases['running'].remove(test)
+
+            logging.debug(testcases)
+
+        if len(testcases['running']) == 0:
+            root.info("Running testcases list is zero, we are done")
+            break
+
+        logging.info('Waiting %s seconds for next heartbeat', str(heartbeat))
+        timeout = timeout - heartbeat
+        time.sleep(heartbeat)
+
+    testresults = []
+    logging.info("Generating junit xml output")
+
+    for test in testcases['finished']:
+        result = test.collect()
+        flow = test.get_flow()
+        testname = flow['name'] + " " + test.get_run_id()
+
+        logging.info("Asserts for " + flow['name'])
+        errors = []
+
+        for k, v in flow['assert'].items():
+            if all(item in result[k].items() for item in flow['assert'][k].items()) is False:
+                errors.append("Failed to assert " + k)
+
+        if errors:
+            tc = TestCase(testname, flow['uuid'], '', errors)
+            tc.add_failure_info(errors)
+            logging.info("Adding failed test")
+            testresults.append(tc)
+        else:
+            logging.info("Adding succesfull test")
+            duration = int(result['executionSummary']['endTime'] - result['executionSummary']['startTime'])
+            tc = TestCase(testname, flow['uuid'], duration / 1000.0, result['executionSummary']['resultStatusType'], '')
+            testresults.append(tc)
+
+    ts = TestSuite('ootests', testresults)
+    with open(args.junitoutput, 'w') as f:
+        TestSuite.to_file(f, [ts], prettyprint=True)
+        logging.info("Writing output to " + args.junitoutput)
 
 
-	parser = argparse.ArgumentParser(description = 'HP Operation Orchestration testing tool')
-	parser.add_argument('--askpass', action='store_true', help='Ignore password from config file and prompt for it.')
-	parser.add_argument('--configfile', default = 'julep.yaml', help='Configfile with hpoo flow testcases')
-	parser.add_argument('--loglevel', default = 'INFO', help='FATAL, ERROR, WARNING, INFO, DEBUG')
-	parser.add_argument('--logfile', default = 'julep.log', help='Logfile to store messages (Default: julep.log)')
-	parser.add_argument('--timeout', default = 3600, type = int, help='The time to wait for flow completion in seconds (Default: 3600 - 1hour)')
-	parser.add_argument('--heartbeat', default = 120, type = int, help='Operation Orchestration polling interval (Default: 120 secs)')
-	parser.add_argument('--quiet', action='store_true', help='Do not print logging to stdout')
-	parser.add_argument('--trustcert', action='store_true', help='Trust self-signed certs')
-	parser.add_argument('--configfmt', default = 'yaml', help="Configfile format - json or yaml. Default json.")
-	parser.add_argument('--delay', default = 15, type = int, help="Delay in seconds to wait between starting flows")
-	parser.add_argument('--junitoutput', default = 'julepout.xml', help="The location of the junit xml output. Default julepout.xml")
+def parse_config(configfile, fmt):
+    f = open(configfile, 'r')
+    txt = f.read()
+    logging.debug(txt)
 
-	args = parser.parse_args()
-	loglevel = levels.get(args.loglevel, logging.NOTSET)
-	logging.basicConfig(
-		level= args.loglevel,
-		format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-		datefmt='%m-%d %H:%M',
-		filename= args.logfile,
-		filemode='a')
+    if fmt == "json":
+        return json.loads(txt)
 
-	root = logging.getLogger()
-
-	if args.quiet is False:
-		console = logging.StreamHandler()
-		console.setLevel(args.loglevel)
-
-		formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
-		console.setFormatter(formatter)
-
-		root.addHandler(console)
-
-	logging.info("Want some blacksea julep?")
-	config = parse_config(args.configfile, args.configfmt)
-	config['general']['trustcert'] = args.trustcert
-
-        if args.askpass is True:
-            config['general']['password'] = getpass.getpass()
-
-
-	testcases = {
-		'running' : [],
-		'finished' : [],
-	}
-
-	for flow in config['flows'] :
-		test = oo.hpoo(config['general'], flow)
-		name = test.run()
-		if args.delay is not None:
-			logging.info("sleeping between runs for %s secs", args.delay)
-			time.sleep(args.delay)
-		testcases['running'].append(test)
-
-	root.debug(testcases)
-
-	timeout = int(args.timeout)
-	heartbeat = int(args.heartbeat)
-
-	while timeout >= heartbeat :
-		logging.info('Tracking testcases in running state')
-		for test in testcases['running'] :
-
-			if test.get_status() == 'RUNNING' :
-				continue
-			else :
-				testcases['finished'].append(test)
-				testcases['running'].remove(test)
-
-			logging.debug(testcases)
-
-		if len(testcases['running']) == 0 :
-			root.info("Running testcases list is zero, we are done")
-			break
-
-		logging.info('Waiting %s seconds for next heartbeat', str(heartbeat))
-		timeout = timeout - heartbeat
-		time.sleep(heartbeat)
-
-	testresults = []
-	logging.info("Generating junit xml output")
-
-	for test in testcases['finished'] :
-		result = test.collect()
-		flow = test.get_flow()
-		testname = flow['name'] + " " + test.get_run_id()
-
-		logging.info("Asserts for "+flow['name'])
-		errors = []
-
-		for k,v in flow['assert'].items() :
-			 if all(item in result[k].items() for item in flow['assert'][k].items()) is False:
-				errors.append("Failed to assert "+k)
-
-		if errors :
-			tc = TestCase(testname,flow['uuid'],'',errors)
-			tc.add_failure_info(errors)
-			logging.info("Adding failed test")
-			testresults.append(tc)
-		else :
-			logging.info("Adding succesfull test")
-			duration = int(result['executionSummary']['endTime'] - result['executionSummary']['startTime'])
-			tc = TestCase(testname,flow['uuid'],duration/1000.0,result['executionSummary']['resultStatusType'],'')
-			testresults.append(tc)
-
-
-	ts = TestSuite('ootests', testresults)
-	with open(args.junitoutput, 'w') as f:
-		TestSuite.to_file(f, [ts], prettyprint=True)
-		logging.info("Writing output to "+args.junitoutput)
-
-
-def parse_config(configfile, fmt) :
-	f = open(configfile,'r')
-	txt = f.read()
-	logging.debug(txt)
-
-	if fmt == "json" :
-		return json.loads(txt)
-
-	return yaml.load(txt)
+    return yaml.load(txt)
 
 
 if __name__ == "__main__":
-	main()
+    main()
